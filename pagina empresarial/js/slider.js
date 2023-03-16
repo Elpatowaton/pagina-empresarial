a811574e1a4088ec67fe7529fa17a30c63355e17
SebasLeon1 <124885452+SebasLeon1@users.noreply.github.com>
2023-03-16 05:38:15 -0500

    Update slider.js

--- pagina empresarial/js/slider.js
+++ pagina empresarial/js/slider.js
@@ -1,2 +1,22 @@
-
+(function(){
+  
+  const sliders = [...document.querySelectorAll('.testimony_body')];
+  const buttonNext = document.querySelector('#next');
+  let value;
+  
+  buttonNext.addEventListener('click', () => {
+    changePosition(1);
+  });
+  
+  buttonBefore.addEventListener('click', ()=>{
+    changePosition(-1);
+  });
+  
+  const changePosition = (add)=>{
+    const currentTestimony = document.querySelector('testimony_body--show').dataset.id
+    value = Number(currentTestimony);
+    value+= add;
+    
+    console.log(value);
+  }
 
